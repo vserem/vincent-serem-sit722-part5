a11562ae9f7f0541589670c8130f8c8e7566c9aa
serem-keli <s223694903@deakin.edu.au>
2024-09-21 01:40:50 +1000

    created shell scripts

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,1 +1,18 @@
-# Your Solution
+#
+# Builds a Docker image.
+#
+# Environment variables:
+#
+#   CONTAINER_REGISTRY - The hostname of your container registry.
+#   VERSION - The version number to tag the images with.
+#
+# Usage:
+#
+#       ./scripts/build-image.sh
+#
+
+set -u # or set -o nounset
+: "$CONTAINER_REGISTRY"
+: "$VERSION"
+
+docker-compose build
