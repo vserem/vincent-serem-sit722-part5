3a1ae1e51a814a739db2600e5de1070ad2e378ea
serem-keli <s223694903@deakin.edu.au>
2024-09-21 18:53:13 +1000

    updated deployment

--- container-registry.tf
+++ container-registry.tf
@@ -8,3 +8,16 @@ resource "azurerm_container_registry" "container_registry" {
   admin_enabled       = true
   sku                 = "Basic"
 }
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_admin_username" {
+  value = azurerm_container_registry.acr.admin_username
+}
+
+output "acr_admin_password" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
+}
