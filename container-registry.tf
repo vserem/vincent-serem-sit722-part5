900601ff8af404c1149089dc919573d8924c06c2
serem-keli <s223694903@deakin.edu.au>
2024-09-21 19:56:00 +1000

    updated container registry

--- container-registry.tf
+++ container-registry.tf
@@ -10,14 +10,14 @@ resource "azurerm_container_registry" "container_registry" {
 }
 
 output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
+  value = azurerm_container_registry.container_registry.login_server
 }
 
 output "acr_admin_username" {
-  value = azurerm_container_registry.acr.admin_username
+  value = azurerm_container_registry.container_registry.admin_username
 }
 
 output "acr_admin_password" {
-  value     = azurerm_container_registry.acr.admin_password
+  value     = azurerm_container_registry.container_registry.admin_password
   sensitive = true
 }
